37578faba1ca69c19813c2b2437c64a6b9ed5b2c
Felipe Accioli <felipeaccioli@gmail.com>
2023-04-06 23:51:19 -0300

    Alterações

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-$('.autoplay').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
+// $('.autoplay').slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+// });
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+$('.carousel').carousel({
+  interval: 3000,
 });
